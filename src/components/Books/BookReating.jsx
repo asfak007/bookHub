020c0960f5d1c336f723694e80e646c0e00058b4
asfak007 <sajalasfakrobin@gmail.com>
2024-05-07 12:18:09 +0600

    make rating array

--- src/components/Books/BookReating.jsx
+++ src/components/Books/BookReating.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const BookReating = () => {
-    
+const BookReating = (rating) => {
+
+    //rating is not a array. transfer it an array and fill it with undefined value by rating value. It like is rating is 4 then the array has 4 undefine value.
+    const starCount = new Array(rating).fill(undefined);
+
     return (
         <>
             <div className="ratting">
