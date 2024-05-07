5d587fcb82de1c27ef230d7c1b0f54658a60bd69
asfak007 <sajalasfakrobin@gmail.com>
2024-05-07 12:33:02 +0600

    fix rating issue

--- src/components/Books/BookReating.jsx
+++ src/components/Books/BookReating.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
-const BookReating = (rating) => {
+const BookReating = ({rating}) => {
 
     //rating is not a array. transfer it an array and fill it with undefined value by rating value. It like is rating is 4 then the array has 4 undefine value.
     const starCount = new Array(rating).fill(undefined);
+    console.log(rating)
 
     return (
         <>
             <div className="ratting">
-                <i className="bi bi-star-fill"/>
-                <i className="bi bi-star-fill"/>
-                <i className="bi bi-star-fill"/>
-                <i className="bi bi-star-fill"/>
+                {
+                    starCount.map((star,index) => (
+                        <i className="bi bi-star-fill" key={index}/>
+                    ))
+                }
+
+
             </div>
         </>
     );
